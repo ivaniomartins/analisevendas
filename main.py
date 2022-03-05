33120e3a413d9b4d11c6fe1118f014afce8616e0
Ivanio Martins <ivaniomartins1991@gmail.com>
2022-03-05 16:27:30 -0300

    Leitura e verificação dos dados

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import pandas as pd
 
+#ler todos os arquivos de vendas
+meses = ['janeiro', 'fevereiro','março', 'abril','mario','junho' ]
+
+
+#lendo cada arquivo e verificando quem bateu a meta
+for mes in meses:
+    tabela_vendas = pd.read_excel(mes + '.xlsx')
+    if (tabela_vendas['Vendas'] >55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] >55000, 'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
+        
