8e70a1a5c9b38439b68015a4da6add571529f201
Ivanio Martins <ivaniomartins1991@gmail.com>
2022-03-05 16:38:08 -0300

    Implementando o Serviço de SMS

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pandas as pd
+from twilio.rest import Client
 
 #ler todos os arquivos de vendas
-meses = ['janeiro', 'fevereiro','março', 'abril','mario','junho' ]
+meses = ['janeiro', 'fevereiro','março', 'abril','maio','junho']
+
+# Your Account SID from twilio.com/console
+account_sid = "AC1c31abffc2ab99c7fabb08a7d6886ac7"
+#"AC1c31abffc2ab99c7fabb08a7d6886ac7"
+# Your Auth Token from twilio.com/console
+auth_token = "5f3f81857a4e1a74d7868dc54c595fe3" #"your_auth_token"
+client = Client(account_sid, auth_token)
 
 
 #lendo cada arquivo e verificando quem bateu a meta
@@ -10,4 +18,11 @@ for mes in meses:
     if (tabela_vendas['Vendas'] >55000).any():
         vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] >55000, 'Vendedor'].values[0]
         vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
-        
+
+
+        message = client.messages.create(
+            to="+5581993753853",
+            from_="+19543680589",
+            body=f'Em {mes}, a meta  foi alcançada pelo vendedor {vendedor}, no valor de R${vendas}')
+
+        print(message.sid)
